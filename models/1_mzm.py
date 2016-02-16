4bf07a5ab4016b84d7fdc4a84ba55f2fe9bf5f2a
zvolsky <zvolsky@seznam.cz>
2016-02-16 07:31:44 +0100

    js/css linking

--- models/1_mzm.py
+++ models/1_mzm.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
+from mzm import P, accept, link
+link('js/mz/mzj', 'css/mz/mzc')
+
 ## if SSL/HTTPS is properly configured and you want all HTTP requests to
 ## be redirected to HTTPS, uncomment the line below:
 if request.is_local:
     from gluon.custom_import import track_changes
     track_changes(True)    # auto-reload modules
-else:
-    request.requires_https()
+#else:
+#    request.requires_https()
---
+++ modules/mzm.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from gluon import *
+
+def P(txt):
+    return txt  # dummy messages translation
+
+def link(*files):
+    for file in files:
+        if not '.' in file:
+            file = '%s.%s' % (file, file.split('/', 1)[0])
+    current.response.files.append(URL('static', file))
+
+def accept(form, ok_msg=P('záznam byl uložen')):
+    """zpracování formuláře s defaultní, explicitní nebo žádnou(None) hláškou po uložení"""
+    if form.process().accepted:
+        if ok_msg:
+            response.flash = ok_msg
+    elif form.errors:
+        response.flash = m.P('prosím, oprav chyby')
